908100458afeb71ff9875b413357a5b6a85f1822
Rasmus Viitanen <rasmus.viitanen@helsinki.fi>
2025-03-19 14:57:08 +0200

    Updated front and back

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
 }
 )
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     if (!body.name || !body.number) {
         return response.status(400).json({
@@ -60,9 +60,11 @@ app.post('/api/persons', (request, response) => {
         name: body.name,
         number: body.number,
     })
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 }
 )
 
@@ -90,6 +92,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
